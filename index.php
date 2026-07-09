18675d0919ef779c4125f18553cd6fee3e41c540
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-07-09 10:24:15 +0500

    php syntacts

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+$array = [
+    'Salom qqlesa',
+    'ikkinchismi',
+    23.33333,
+    true
+];
+
+$array2 = array('Salom qqlesa',
+    'ikkinchismi',
+    23.33333,
+    true
+);
+
+//print_r($array);
+
+
+class Car
+{
+    public $color;
+    public $model;
+
+    public function __construct($color, $model)
+    {
+        $this->color = $color;
+        $this->model = $model;
+    }
+
+    public function message()
+    {
+        return "My car is a " . $this->color . " " .$this->model . "!";
+    }
+}
+
+$myCar = new Car("Black", "BYD");
+
+echo $myCar->message();
+
+
+?>
+
