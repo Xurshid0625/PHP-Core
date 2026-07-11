91653b26c3494d0f99f9769b2a9621045362248b
Xurshid Abdumannonov <abdumannonovxurshid0625@gmail.com>
2026-07-11 17:54:03 +0500

    php

--- index.php
+++ index.php
@@ -1,162 +1,62 @@
 <?php
 
-$array = [
-    'Salom qqlesa',
-    'ikkinchismi',
-    23.33333,
-    true
-];
+$host = "localhost";
+$username = "postgres";
+$password = "postgres";
+$dbname = "mydb";
 
-$array2 = array('Salom qqlesa',
-    'ikkinchismi',
-    23.33333,
-    true
-);
-
-//print_r($array);
-
-
-class Car
-{
-    public $color;
-    public $model;
-
-    public function __construct($color, $model)
-    {
-        $this->color = $color;
-        $this->model = $model;
-    }
-
-    public function message()
-    {
-        return "My car is a " . $this->color . " " .$this->model . "!";
-    }
+try {
+    $connect = new PDO("pgsql:host=$host;dbname=$dbname", $username, $password);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "connect failed:" . $e->getMessage();
 }
 
-$myCar = new Car("Black", "BYD");
-
-//echo $myCar->message();
-
-
-$gap = 'salom, mening ismim Xurshid';
-
-//echo strlen($gap);
-
-//echo str_replace('mening', 'my', $gap);
-
-//echo str_word_count($gap);
-
-//echo strrev($gap);
-
-//echo strpos($gap, 'mening');
-
-//
-//$math = rand(1, 100);
-//
-//echo $math;
-
-//
-//define('RANG', 'yashil');
-//
-//echo RANG;
-
-$a = 20;
-$b = 201;
-
-//$value = $a + $b;
-//
-//echo $value;
-//
-//if($a == $b) {
-//    echo 'a teng b ga';
-//} elseif($a > $b) {
-//    echo 'a b dan katta';
-//} else {
-//    echo 'a teng emas b ga';
-//}xurshid.jira2006
-//
-//$rang = 'red';
-//
-//switch($rang) {
-//    case 'red';
-//        echo 'qizil';
-//        break;
-//    case 'greee';
-//        echo 'yashil';
-//        break;
-//   default;
-//         echo 'yoq';
-//}
-
-//for ($x = 1; $x <= 100; $x++) {
-//    if ($x == 6) {
-//        continue;
-//    }
-//    echo $x . '<br>';
-//}
-
-//$x = 1;
-//while($x <= 10) {
-//    if ($x == 8) {
-//        break;
-//    }
-//    echo $x . '<br>';
-//    $x++;
-//}
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $title = $_POST['title'];
+    $body = $_POST['body'];
 
 
-//$sons = [1, 2, 3, 4, 5];
-//
-//$age = array("ali"=>"20", "asil"=>"22");
-//
-//foreach($age as $name => $age) {
-//    echo $name . ' - ' . $age . '<br>';
-//}
+    $statement = $connect->prepare("INSERT INTO posts (title, body) VALUES (:title, :body)");
 
-//$x = 1;
-//do {
-//    echo $x . '<br>';
-//    $x++;
-//} while ($x <= 5);
+    $statement->execute([
+            'title' => $title,
+            'body' => $body,
+    ]);
+}
 
-//functions
-
-//function names($name) {
-//    echo 'ism' . ' ' . $name . 'ga teng' . '<br>';
-//}
-//
-//names('Xurshid');
-//names('Ali');
-
-//function qoshish($x, $y)
-//{
-//
-//    $result =  $x + $y;
-//    return $result;
-//}
-//
-//$nat = qoshish(20, 30);
-// echo $nat;
-
-// Array
-
-//var_dump($_SERVER['SERVER_SOFTWARE']);
-
-//$ovqats = ['ssdd', 'ssdd', 'ssdd', 'ssdd'];
-//
 ?>
-<!---->
-<!--<!DOCTYPE html>-->
-<!--<html>-->
-<!--<head>-->
-<!--    <title>Page Title</title>-->
-<!--</head>-->
-<!--<body>-->
-<!--<ul>-->
-<!--    --><?php //foreach($ovqats as $ovqat): ?>
-<!--    <h1>--><?php //= $ovqat ?><!--</h1>-->
-<!--    --><?php //endforeach; ?>
-<!--</ul>-->
-<!---->
-<!--</body>-->
-<!--</html>-->
+
+
+
+<?php
+
+$statement = $connect->prepare("SELEct * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll();
+?>
+
+
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Page Title</title>
+    </head>
+    <body>
+    <ul>
+        <form method="POST" action="">
+            <div style="margin-bottom: 40px">
+                <input name="title" type="text">
+            </div>
+
+            <div>
+                <input name="body" type="text">
+            </div>
+            <button type="submit">Yubor</button>
+        </form>
+    </ul>
+
+    </body>
+    </html>
